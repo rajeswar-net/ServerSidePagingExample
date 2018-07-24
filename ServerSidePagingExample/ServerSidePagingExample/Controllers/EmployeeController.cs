ea2afeb084344bc64045e9eca76753635f5dd342
Rajeswara <rajeswar.net@gmail.com>
2018-07-24 09:53:32 -0500

    Working Example with Employee Tracker Table

---
+++ ServerSidePagingExample/ServerSidePagingExample/Controllers/EmployeeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServerSidePagingExample.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeTrackerEntities _emplpoyeeEntity;
+        public EmployeeController()
+        {
+            _emplpoyeeEntity = new EmployeeTrackerEntities();
+        }
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoadEmployeeData()
+        {
+            try
+            {
+                var draw = Request.Form.GetValues("draw").FirstOrDefault();
+                var start = Request.Form.GetValues("start").FirstOrDefault();
+                var length = Request.Form.GetValues("length").FirstOrDefault();
+                var sortColumn = Request.Form.GetValues("columns[" + Request.Form.GetValues("order[0][column]").FirstOrDefault() + "][name]").FirstOrDefault();
+                var sortColumnDirection = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+                var searchValue = Request.Form.GetValues("search[value]").FirstOrDefault();
+
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+                int recordsTotal = 0;
+
+                var employeeData = (from e in _emplpoyeeEntity.employees
+                                    select new
+                                    {
+                                        employee_id = e.employee_id,
+                                        first_name = e.first_name,
+                                        last_name = e.last_name,
+                                        sex = e.sex,
+                                        salary = Math.Round(e.salary, 2),
+                                        start_date =  e.start_date,
+                                        age=e.age
+                                    });
+
+                if (!string.IsNullOrEmpty(sortColumn) && !string.IsNullOrEmpty(sortColumnDirection))
+                {
+                    employeeData = employeeData.OrderBy(sortColumn + " " + sortColumnDirection);
+                }
+
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    employeeData = employeeData.Where(e => e.first_name.Contains(searchValue) || e.last_name.Contains(searchValue));
+                }
+                recordsTotal = employeeData.Count();
+                var finalData = employeeData.Skip(skip).Take(pageSize).ToList();
+
+                return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = finalData });
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
--- ServerSidePagingExample/ServerSidePagingExample/Scripts/salesordercustom.js
+++ ServerSidePagingExample/ServerSidePagingExample/Scripts/salesordercustom.js
@@ -18,7 +18,7 @@
         "serverSide": true,
         "ajax":
       {
-          "url": "/Plugin/GetData",
+          "url": "/Home/getSalesOrders",
           "type": "POST",
           "dataType": "JSON"
       },
@@ -45,4 +45,64 @@
               "data": "UnitPriceDiscount"
           }]
     });
-});
+
+    $("#employeeGrid").DataTable({  
+  
+        "processing": true, // for show progress bar  
+        "serverSide": true, // for process server side  
+        "filter": true, // this is for disable filter (search box)  
+        "orderMulti": false, // for disable multiple column at once  
+        "pageLength": 5,  
+  
+        "ajax": {  
+            "url": "/Employee/LoadEmployeeData",  
+            "type": "POST",  
+            "datatype": "json"  
+        },  
+  
+        "columnDefs":  
+        [{  
+            "targets": [0],  
+            "visible": false,  
+            "searchable": false  
+        }
+        //,  
+        //{  
+        //    "targets": [7],  
+        //    "searchable": false,  
+        //    "orderable": false  
+        //},  
+        //{  
+        //    "targets": [8],  
+        //    "searchable": false,  
+        //    "orderable": false  
+        //},  
+        //{  
+        //    "targets": [9],  
+        //    "searchable": false,  
+        //    "orderable": false  
+            //}
+        ],  
+  
+        "columns": [  
+              { "data": "employee_id", "name": "employee_id", "autoWidth": true },  
+              { "data": "first_name", "name": "first_name", "autoWidth": true },  
+              { "data": "last_name", "name": "ContactName", "autoWidth": true },  
+              { "data": "sex", "name": "sex", "autoWidth": true },  
+              { "data": "age", "name": "age", "autoWidth": true },  
+              { "data": "start_date", "name": "start_date", "autoWidth": true },  
+              { "data": "salary", "name": "salary", "autoWidth": true },  
+              {  
+                  "render": function (data, type, full, meta)  
+                  { return '<a class="btn btn-info" href="/Employee/Edit/' + full.employee_id + '">Edit</a>'; }  
+              },  
+               {  
+                   data: null, render: function (data, type, row) {  
+                       return "<a href='#' class='btn btn-danger' onclick=DeleteData('" + row.employee_id + "'); >Delete</a>";  
+                   }  
+               },  
+  
+        ]  
+  
+    });  
+});  
