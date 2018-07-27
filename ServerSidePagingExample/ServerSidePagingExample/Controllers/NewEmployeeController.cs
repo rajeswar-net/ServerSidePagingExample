2631c2f18749562d5b3da32b017e2b1ccf6e6689
Rajeswara <rajeswar.net@gmail.com>
2018-07-27 08:36:42 -0500

    Checked in with PDF functionality using Rotativa

---
+++ ServerSidePagingExample/ServerSidePagingExample/Controllers/NewEmployeeController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ServerSidePagingExample;
+
+namespace ServerSidePagingExample.Controllers
+{
+    public class NewEmployeeController : Controller
+    {
+        private EmployeeTrackerEntities db = new EmployeeTrackerEntities();
+
+        // GET: NewEmployee
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.employees.ToListAsync());
+        }
+        public async Task<ActionResult> DownloadViewPDF()
+        {
+            var model = await db.employees.ToListAsync();
+            //Code to get content
+            return new Rotativa.ViewAsPdf("Index", model) { FileName = "Employees.pdf" };
+}
+        // GET: NewEmployee/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            employee employee = await db.employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: NewEmployee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NewEmployee/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "employee_id,first_name,last_name,position_id,office_id,sex,age,start_date,salary,updated_utc")] employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.employees.Add(employee);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: NewEmployee/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            employee employee = await db.employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: NewEmployee/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "employee_id,first_name,last_name,position_id,office_id,sex,age,start_date,salary,updated_utc")] employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: NewEmployee/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            employee employee = await db.employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: NewEmployee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            employee employee = await db.employees.FindAsync(id);
+            db.employees.Remove(employee);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
